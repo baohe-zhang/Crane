ff94aa18bd4cdad6921c51543fbf082db6dc2dcf
baohe-zhang <baohez2@illinois.edu>
2018-11-24 23:43:43 -0600

    contractor

--- core/contractor/contractor.go
+++ core/contractor/contractor.go
@@ -7,6 +7,7 @@ import (
 	// "math/rand"
 	"encoding/json"
 	"net"
+	"crane/core/messages"
 )
 
 const (
@@ -22,6 +23,15 @@ type Contractor struct {
 	tuples chan []interface{}
 	results chan []interface{}
 	port string
+	pub *messages.Publisher
+	subs []*messages.Subscriber
+	preGrouping string
+	preField int
+	sucGrouping string
+	sucField int
+	sucIndexMap map[int]string
+	rwmutex sync.RWMutex
+	wg sync.WaitGroup
 }
 
 type Worker struct {
@@ -32,7 +42,8 @@ type Worker struct {
 	variables []interface{}
 }
 
-func NewContractor(numWorkers int, procFunc ProcessorFunc, port string) *Contractor {
+func NewContractor(numWorkers int, procFunc ProcessorFunc, port string, subAddrs []string, 
+					preGrouping string, preField int, sucGrouping string, sucField int) *Contractor {
 	tuples := make(chan []interface{}, BUFLEN)
 	results := make(chan []interface{}, BUFLEN)
 
@@ -50,12 +61,29 @@ func NewContractor(numWorkers int, procFunc ProcessorFunc, port string) *Contrac
 		workers = append(workers, worker)
 	}
 
+	// Create publisher and subscribers
+	publisher := messages.NewPublisher(":"+port)
+	subscribers := make([]*messages.Subscriber, 0)
+	for _, subAddr := range subAddrs {
+		subscribers = append(subscribers, messages.NewSubscriber(subAddr))
+	}
+
+	// Record the index of successor
+	sucIndexMap := make(map[int]string)
+
 	c := &Contractor{
 		numWorkers: numWorkers,
 		workers: workers,
 		tuples: tuples,
 		results: results,
 		port: port,
+		pub: publisher,
+		subs: subscribers,
+		preGrouping: preGrouping,
+		preField: preField,
+		sucGrouping: sucGrouping,
+		sucField: sucField,
+		sucIndexMap: sucIndexMap,
 	}
 
 	return c
@@ -66,90 +94,66 @@ func (c *Contractor) Start() {
 	defer close(c.results)
 
 	fmt.Println("contractor start")
-	go c.setupListener()
+
+	// Start subscriber
+	for _, sub := range c.subs {
+		go sub.ReadMessage()
+	}
+	// Start publisher
+	go c.pub.AcceptConns()
+	go c.pub.PublishMessage(c.pub.PublishBoard)
+
+	c.wg.Add(1)
+	go c.buildSucIndexMap()
+	c.wg.Wait()
+
+	go c.receiveTuple()
 	go c.distributeTuple()
 	go c.outputTuple()
 
-	var wg sync.WaitGroup
-	wg.Add(1)
-	wg.Wait()
+	c.wg.Add(1)
+	c.wg.Wait()
 }
 
-func (c *Contractor) setupListener() {
-	listener, err := net.Listen("tcp", ":"+c.port)
-	fmt.Println("listening on port " + c.port)
-	if err != nil {
-		fmt.Println(err.Error())
+func (c *Contractor) receiveTuple() {
+	for _, sub := range c.subs {
+		go func() {
+			for msg := range sub.PublishBoard {
+				var tuple []interface{}
+				json.Unmarshal(msg.Payload, &tuple)
+				c.tuples <- tuple
+			}
+		}()
 	}
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-		go c.receiveTuple(conn)
-	}
-}
-
-func (c *Contractor) receiveTuple(conn net.Conn) {
-	defer conn.Close()
-
-	for {
-		buf := make([]byte, BUFFSIZE)
-		n, err := conn.Read(buf)
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-		var tuple []interface{}
-		json.Unmarshal(buf[:n], &tuple)
-		c.tuples <- tuple
-	}
-
-	// words := []string{"one", "two", "three", "four", "five", "six"}
-	// for i := 0; i < 1000; i++ {
-	// 	tuple := make([]interface{}, 0)
-	// 	tuple = append(tuple, words[rand.Intn(len(words))])
-	// 	c.tuples <- tuple
-	// 	time.Sleep(10 * time.Millisecond)
-	// }
 }
 
 func (c *Contractor) distributeTuple() {
-	switch grouping := "fields"; grouping {
+	switch c.preGrouping {
 	case "shuffle":
-		for {
-			select {
-			case tuple := <-c.tuples:
-				processed := false
-				for !processed {
-					// Round-Robin distribute
-					for _, worker := range c.workers {
-						if worker.available {
-							go worker.processTuple(tuple)
-							processed = true
-							break
-						}
-					}
-				}
-			default:
-			}
-		}
-
-	case "fields":
-		for {
-			select {
-			case tuple := <-c.tuples:
-				// distribute by fields
-				wid := hash(tuple[0]) % c.numWorkers
-				worker := c.workers[wid]
-				processed := false
-				for !processed {
+		for tuple := range c.tuples {
+			processed := false
+			for !processed {
+				// Round-Robin distribute
+				for _, worker := range c.workers {
 					if worker.available {
 						go worker.processTuple(tuple)
 						processed = true
 						break
 					}
 				}
-			default:
+			}
+		}
+	case "byFields":
+		for tuple := range c.tuples {
+			wid := hash(tuple[c.preField]) % c.numWorkers
+			worker := c.workers[wid]
+			processed := false
+			for !processed {
+				if worker.available {
+					go worker.processTuple(tuple)
+					processed = true
+					break
+				}
 			}
 		}
 	default:
@@ -162,21 +166,61 @@ func (w *Worker) processTuple(tuple []interface{}) {
 	// fmt.Printf("worker (%d) process tuple (%v)\n", w.id, tuple)
 	var result []interface{}
 	w.procFunc(tuple, &result, &w.variables)
-	fmt.Printf("worker (%d) output tuple (%v)\n", w.id, result)
+	fmt.Printf("worker %d output tuple (%v)\n", w.id, result)
 	w.results <- result
 
 	w.available = true
 }
 
 func (c *Contractor) outputTuple() {
-	for {
-		select {
-		case <- c.results:
-			// fmt.Printf("output tuple (%v)\n", result)
-
-		default:
+	switch c.sucGrouping {
+	case "shuffle":
+		count := 0
+		for result := range c.results {
+			bin, _ := json.Marshal(result)
+			sucid := count % len(c.sucIndexMap)
+			c.rwmutex.RLock()
+			sucConnId := c.sucIndexMap[sucid]
+			c.rwmutex.RUnlock()
+			c.pub.PublishBoard <- messages.Message{
+				Payload: bin,
+				TargetConnId: sucConnId,
+			}
+			count++
 		}
+	case "byFields":
+		for result := range c.results {
+			bin, _ := json.Marshal(result)
+			sucid := hash(result[c.sucField]) % len(c.sucIndexMap)
+			c.rwmutex.RLock()
+			sucConnId := c.sucIndexMap[sucid]
+			c.rwmutex.RUnlock()
+			c.pub.PublishBoard <- messages.Message{
+				Payload: bin,
+				TargetConnId: sucConnId,
+			}
+		}
+	case "all":
+		for result := range c.results {
+			bin, _ := json.Marshal(result)
+			c.pub.Pool.Range(func(id string, conn net.Conn) {
+				c.pub.PublishBoard <- messages.Message{
+					Payload: bin,
+					TargetConnId: id,
+				}
+			})
+		}
+	default:
 	}
+}
+
+func (c *Contractor) buildSucIndexMap() {
+	c.pub.Pool.Range(func(id string, conn net.Conn) {
+		c.rwmutex.Lock()
+		c.sucIndexMap[len(c.sucIndexMap)] = id
+		c.rwmutex.Unlock()
+	})
+	c.wg.Done()
 }
 
 func processorFunc(tuple []interface{}, result *[]interface{}, variables *[]interface{}) error {
@@ -205,7 +249,7 @@ func processorFunc(tuple []interface{}, result *[]interface{}, variables *[]inte
 
 
 func main() {
-	contractor := NewContractor(10, processorFunc, "5000")
+	contractor := NewContractor(10, processorFunc, "5000", []string{"127.0.0.1:5001"}, "byFields", 0, "all", 0)
 	contractor.Start()
 }
 
