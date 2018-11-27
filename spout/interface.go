d05a9604fff1bc5baaa7c0707bb01bffb85f52ed
baohe-zhang <baohez2@illinois.edu>
2018-11-27 16:52:07 -0600

    cyy

--- spout/interface.go
+++ spout/interface.go
@@ -1,34 +1,34 @@
-package spout
+package main
 
-import ()
+// import ()
 
-type SpoutOutputCollector struct {
-}
+// type SpoutOutputCollector struct {
+// }
 
-type OutputFieldsDeclarer struct {
-}
+// type OutputFieldsDeclarer struct {
+// }
 
-type Spout interface {
-	Open()
-	NextTuple() []interface{}
-}
+// type Spout interface {
+// 	Open()
+// 	NextTuple() []interface{}
+// }
 
-type SpoutInst struct {
-	SpoutName    string
-	InputFile    string
-	PluginFile   string
-	PluginSymbol string
-	GroupingHint string
-	FieldIndex   int
-	InstNum      int
-}
+// type SpoutInst struct {
+// 	SpoutName    string
+// 	InputFile    string
+// 	PluginFile   string
+// 	PluginSymbol string
+// 	GroupingHint string
+// 	FieldIndex   int
+// 	InstNum      int
+// }
 
-func NewSpoutInst(name, pluginFile, pluginSymbol string, grouping string, mainField int) *SpoutInst {
-	spoutInst := &SpoutInst{}
-	spoutInst.SpoutName = name
-	spoutInst.PluginFile = pluginFile
-	spoutInst.PluginSymbol = pluginSymbol
-	spoutInst.GroupingHint = grouping
-	spoutInst.FieldIndex = mainField
-	spoutInst.InstNum = 1
-}
+// func NewSpoutInst(name, pluginFile, pluginSymbol string, grouping string, mainField int) *SpoutInst {
+// 	spoutInst := &SpoutInst{}
+// 	spoutInst.SpoutName = name
+// 	spoutInst.PluginFile = pluginFile
+// 	spoutInst.PluginSymbol = pluginSymbol
+// 	spoutInst.GroupingHint = grouping
+// 	spoutInst.FieldIndex = mainField
+// 	spoutInst.InstNum = 1
+// }
