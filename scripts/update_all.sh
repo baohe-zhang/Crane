9c47839e06801db494541f54333b790b00728a4f
Kechen Lu <kechenl3@illinois.edu>
2018-11-28 21:10:32 -0600

    update
    Signed-off-by: Kechen Lu <kechenl3@illinois.edu>

--- scripts/update_all.sh
+++ scripts/update_all.sh
@@ -3,7 +3,7 @@
 for val in 0{1..9} 10
 do
     echo VM$val
-    ssh kechenl3@fa18-cs425-g29-$val.cs.illinois.edu "cd ~/go/src/crane; git pull; cd ./core/driver ; go build; cd ../supervisor/; go build; cd ../process/;go build -buildmode=plugin; exit"
+    ssh kechenl3@fa18-cs425-g29-$val.cs.illinois.edu "cd ~/go/src/crane; git pull; cd ./core/driver ; go build; cd ../supervisor/; go build; cd ../../tools/sdfs_client/;go build; exit"
 done
 echo 'Git Update!'
 
