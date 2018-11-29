c3c9962e382751f511cb6eacfe7c2c2358292548
baohe-zhang <baohez2@illinois.edu>
2018-11-28 23:18:53 -0600

    xxx

--- core/supervisor/interface.go
+++ core/supervisor/interface.go
@@ -53,7 +53,7 @@ func (s *Supervisor) StartDaemon() {
 				task := &utils.BoltTaskMessage{}
 				utils.Unmarshal(payload.Content, task)
 				if task.PluginFile != "None" {
-					go s.GetFile(task.PluginFile)
+					// go s.GetFile(task.PluginFile)
 				}
 				bw := boltworker.NewBoltWorker(10, "./"+task.PluginFile, task.Name, task.Port, task.PrevBoltAddr,
 					task.PrevBoltGroupingHint, task.PrevBoltFieldIndex,
@@ -66,7 +66,7 @@ func (s *Supervisor) StartDaemon() {
 				task := &utils.SpoutTaskMessage{}
 				utils.Unmarshal(payload.Content, task)
 				if task.PluginFile != "None" {
-					go s.GetFile(task.PluginFile)
+					// go s.GetFile(task.PluginFile)
 				}
 				sw := spoutworker.NewSpoutWorker("./"+task.PluginFile, task.Name, task.Port, task.GroupingHint, task.FieldIndex)
 				s.SpoutWorkers = append(s.SpoutWorkers, sw)
