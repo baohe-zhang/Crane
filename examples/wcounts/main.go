3ad835d80e58e8c3a93623c9dd728d6a434d0247
baohe-zhang <baohez2@illinois.edu>
2018-11-28 21:19:12 -0600

    x

--- examples/wcounts/main.go
+++ examples/wcounts/main.go
@@ -12,16 +12,17 @@ func main() {
 	tm := topology.Topology{}
 
 	// Create a spout
-	sp := spout.NewSpoutInst("NextTuple", "../process/process.so", "NextTuple", utils.GROUPING_BY_FIELD, 0)
+	sp := spout.NewSpoutInst("NextTuple", "process.so", "NextTuple", utils.GROUPING_BY_FIELD, 0)
 	sp.SetInstanceNum(1)
 	tm.AddSpout(sp)
 
 	// Create a bolt
 	// Params: name, pluginFile, pluginSymbol, groupingHint, fieldIndex
-	bm := bolt.NewBoltInst("ProcFunc", "../process/process.so", "ProcFunc", utils.GROUPING_BY_ALL, 0)
+	bm := bolt.NewBoltInst("ProcFunc", "process.so", "ProcFunc", utils.GROUPING_BY_ALL, 0)
 	bm.SetInstanceNum(2)
 	bm.AddPrevTaskName("NextTuple")
 	tm.AddBolt(bm)
 
+	tm.SubmitFile("./process.so", "process.so")
 	tm.Submit(":5050")
 }
