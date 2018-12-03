0831eef2f62a78187d3c323221193b3a0f0f82d8
baohe-zhang <baohez2@illinois.edu>
2018-12-02 19:49:24 -0600

    new wc

---
+++ examples/wcounts/process/process.go
@@ -0,0 +1,138 @@
+package main 
+
+import (
+	// "fmt"
+	"log"
+	"time"
+	"errors"
+	// "os"
+	// "bufio"
+)
+
+// Sample word count bolt
+func ProcFunc(tuple []interface{}, result *[]interface{}, variables *[]interface{}) error {
+	// Bolt's global variables
+	var countMap map[string]interface{}
+	if (len(*variables) == 0) {
+		// Initialize variables
+		countMap = make(map[string]interface{})
+		*variables = append(*variables, countMap)
+	}
+	countMap = (*variables)[0].(map[string]interface{})
+
+	// Bolt's process logic
+	word := tuple[0].(string)
+	_, ok := countMap[word]
+	if !ok {
+		countMap[word] = float64(0)
+	}
+	countMap[word] = countMap[word].(float64) + 1
+	*result = []interface{}{word, countMap[word].(float64)}
+	log.Printf("Bolt Emit: (%v)\n", *result)
+
+	return nil
+}
+
+// Sample word generator
+func NextTuple(tuple []interface{}, result *[]interface{}, variables *[]interface{}) error {
+	// Variables
+	words := []string{"china", "usa", "japan", "korea", "russia", "india", "singapore", "canada", "mexico", "france", "germany", "greek", "finland"}
+	var counterMap map[string]interface{}
+
+	if (len(*variables) == 0) {
+		// Initialize variables
+		counterMap = make(map[string]interface{})
+		*variables = append(*variables, counterMap)
+		counterMap["counter"] = float64(0)
+	}
+	counterMap = (*variables)[0].(map[string]interface{})
+
+	// Logic
+	if counterMap["counter"].(float64) < 1000 {
+		log.Printf("Spout Counter %v\n", counterMap["counter"])
+		*result = []interface{}{words[int(counterMap["counter"].(float64)) % len(words)]}
+		log.Printf("Spout Emit: (%v)\n", *result)
+		counterMap["counter"] = counterMap["counter"].(float64) + 1
+	}
+	time.Sleep(10 * time.Millisecond)
+
+	// Return value
+	if (len(*result) > 0) {
+		return nil
+	} else {
+		return errors.New("next tuple is nil")
+	}
+}
+
+
+
+// func ProcFunc(tuple []interface{}, result *[]interface{}, variables *[]interface{}) error {
+// 	num := tuple[0].(float64)
+// 	num *= 2
+// 	fmt.Println("bolt emit: ", num)
+// 	*result = []interface{}{num}
+
+// 	return nil
+// }
+
+// // Sample counter
+// func NextTuple(tuple []interface{}, result *[]interface{}, variables *[]interface{}) error {
+// 	var counter *float64
+// 	if (len(*variables) == 0) {
+// 		counter = new(float64)
+// 		*variables = append(*variables, counter)
+// 	}
+// 	counter = ((*variables)[0]).(*float64)
+
+// 	if *counter < 101 {
+// 		fmt.Println("spout emit: ", *counter)
+// 		*result = []interface{}{*counter}
+// 		(*counter)++
+// 	}
+
+// 	time.Sleep(100 * time.Millisecond)
+
+// 	if (len(*result) > 0) {
+// 		return nil
+// 	} else {
+// 		return errors.New("next tuple is nil")
+// 	}
+// }
+
+// // Sample File Reader
+// func NextTuple(tuple []interface{}, result *[]interface{}, variables *[]interface{}) error {
+// 	// Create or get state variables
+// 	var reader *bufio.Reader
+// 	if (len(*variables) == 0) {
+// 		file, err := os.Open("/Users/zhangbaohe/go/src/crane/core/process/integers.txt")
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			os.Exit(1)
+// 		}
+// 		reader = bufio.NewReader(file)
+// 		*variables = append(*variables, reader)
+// 	}
+// 	reader = ((*variables)[0]).(*bufio.Reader)
+
+// 	// Generate next tuple
+// 	b, err := reader.ReadByte()
+// 	if err != nil {
+// 		return errors.New("next tuple is nil")
+// 	}
+
+// 	*result = []interface{}{float64(b)}
+
+// 	time.Sleep(100 * time.Millisecond)
+
+// 	return nil
+// }
+
+
+// // Sample Multiply Two
+// func processorFunc(tuple []interface{}, result *[]interface{}, variables *[]interface{}) error {
+// 	num := tuple[0].(float64)
+// 	num *= 2
+// 	*result = []interface{}{num}
+
+// 	return nil
+// }
