a3fa3a765b0c7331097de31649e3a46587e7f20e
baohe-zhang <baohez2@illinois.edu>
2018-12-03 15:25:20 -0600

    xxx

--- scripts/update_all.sh
+++ scripts/update_all.sh
@@ -3,7 +3,7 @@
 for val in 0{1..9} 10
 do
     echo VM$val
-    ssh kechenl3@fa18-cs425-g29-$val.cs.illinois.edu "cd ~/go/src/crane_pure; git reset --hard; git pull; cd ./core/driver ; go build; cd ../supervisor/; go build; cd ../../tools/sdfs_client/;go build; exit"
+    ssh kechenl3@fa18-cs425-g29-$val.cs.illinois.edu "cd ~/go/src/crane; git reset --hard; git pull; cd ./core/driver ; go build; cd ../supervisor/; go build; cd ../../tools/sdfs_client/;go build; exit"
 done
 echo 'Git Update!'
 
